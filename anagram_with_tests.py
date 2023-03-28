d6f7e3d51587069508b3f8c622a9c9e9a9d47af7
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-03-28 12:07:50 +0100

    True test case added

--- anagram_with_tests.py
+++ anagram_with_tests.py
@@ -9,3 +9,11 @@ def is_anagram(first_word, second_word):
 print(is_anagram("leap", "pale"))  # Should output True
 print(is_anagram("frog", "fog"))  # Should output False
 print(is_anagram("Iamlordvoldemort", "TomMarvoloRiddle"))  # Should output True
+
+
+class TestAnagrams(TestCase):
+
+    def test_is_anagram_true(self):
+        self.assertTrue(is_anagram("dog", "god"))
+        self.assertTrue(is_anagram("angel", "glean"))
+
