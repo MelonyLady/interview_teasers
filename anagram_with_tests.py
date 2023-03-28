c2c171f2769dad45ad6ff21f6bb9c773cc642e99
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-03-28 12:04:22 +0100

    added manual tests

--- anagram_with_tests.py
+++ anagram_with_tests.py
@@ -1,3 +1,8 @@
 def is_anagram(first_word, second_word):
     return sorted(first_word.casefold()) == sorted(second_word.casefold())
 
+
+# manual testing
+print(is_anagram("leap", "pale"))  # Should output True
+print(is_anagram("frog", "fog"))  # Should output False
+print(is_anagram("Iamlordvoldemort", "TomMarvoloRiddle"))  # Should output True
