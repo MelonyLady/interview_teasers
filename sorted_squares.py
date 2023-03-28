7258192afa3e59dd54feccc078066088df108a16
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-03-28 11:53:20 +0100

    added manual tests for computation

--- sorted_squares.py
+++ sorted_squares.py
@@ -6,9 +6,19 @@ def sorted_squares(numbers):
 
 # manual tests
 
+
 print(sorted_squares([1, 2, 3, 5, 6, 8, 9]))
 print(sorted_squares([10, 7, 65, 2, 3, 6]))
 print(sorted_squares([-10, 25, -65, 3, 10]))
+print("************* End Sorted Squares *******************")
+
 
 def sorted_squares_comp(numbers):
     return sorted([n **2 for n in numbers])
+
+# manual tests
+
+
+print(sorted_squares_comp([-10, 25, -65, 3, 10]))
+print(sorted_squares_comp([10, 7, 65, 2, 3, 6]))
+print(sorted_squares_comp([1, 2, 3, 5, 6, 8, 9]))
