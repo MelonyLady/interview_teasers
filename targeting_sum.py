f7794f4ccb701ecc12a5343c82a87880d5cbd781
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-03-28 11:58:48 +0100

    added instructions for task

---
+++ targeting_sum.py
@@ -0,0 +1,11 @@
+# Question instructions:
+""" You will be asked to write a function that will accept an array of numbers and an integer
+representing a target sum.
+
+If any two numbers from the accepted numbers sum up to the target sum, then your
+function should return them in an array, in any order. If no two numbers sum up to the
+target sum, the function should return an empty array.
+
+Note that the target sum has to be obtained by summing two different numbers in the
+#array. You cannot add a single integer to itself in order to obtain the target sum."""
+
