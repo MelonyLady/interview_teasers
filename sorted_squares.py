244a408fa801965e7cf86e52947afc7c1a647d55
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-03-28 11:49:28 +0100

    manual tests added

--- sorted_squares.py
+++ sorted_squares.py
@@ -3,3 +3,9 @@ def sorted_squares(numbers):
     for number in numbers:
         square_numbers.append(number ** 2)
     return sorted(square_numbers)
+
+# manual tests
+
+print(sorted_squares([1, 2, 3, 5, 6, 8, 9]))
+print(sorted_squares([10, 7, 65, 2, 3, 6]))
+print(sorted_squares([-10, 25, -65, 3, 10]))
