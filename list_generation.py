a38fc91b5bb009047c406243a0ba0855c3db5316
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-05 14:21:16 +0100

    initial code work forinstructions

--- list_generation.py
+++ list_generation.py
@@ -1,3 +1,15 @@
 # Accept a string and use list comprehension to generate as many strings of that word
 # in an array as many letters are in the given string. For example:
 # string = "Code" (4 letters in the string) --> ["Code", "Code", "Code", "Code"]
+
+
+def create_list(morewords):
+    numchar = len(morewords)
+    result = []
+    for letter in range(numchar):
+        result.append(morewords)
+    return result
+
+
+print(create_list("Code"))
+print(create_list("lovely"))
