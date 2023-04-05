fd79b08e160f6dfea00bfae6716edcac8a4a3978
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-05 14:17:56 +0100

    instructions

---
+++ list_generation.py
@@ -0,0 +1,3 @@
+# Accept a string and use list comprehension to generate as many strings of that word
+# in an array as many letters are in the given string. For example:
+# string = "Code" (4 letters in the string) --> ["Code", "Code", "Code", "Code"]
