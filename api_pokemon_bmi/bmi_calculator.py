5dfd6ea068d3a718d443d1c063516bac4b04e61f
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-05 15:12:43 +0100

    refactored file, added functions to bmi

--- api_pokemon_bmi/bmi_calculator.py
+++ api_pokemon_bmi/bmi_calculator.py
@@ -1,6 +1,20 @@
-import pokemon_api
+import pokemon
 
 
 def calculate_bmi(weight, height):
     return round(weight / height ** 2, 2)
 
+
+def get_health_feedback(bmi):
+    if bmi < 18:
+        return 'underweight'
+    elif bmi < 25:
+        return 'ideal weight for height'
+    else:
+        return 'overweight'
+
+
+if __name__ == '__main__':
+    pokemon = pokemon.get_random_pokemon()
+    pokemon_bmi = calculate_bmi(pokemon['weight'], pokemon['height'])
+    print(f"The BMI of {pokemon_api['name']} is {get_health_feedback(pokemon_bmi)} when compared to human standards.")
