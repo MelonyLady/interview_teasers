fdaabef26c951a9faaf47542b3a854a52eca348b
Sydney Humayun <psychdreamer@hotmail.co.uk>
2023-04-05 14:40:47 +0100

    get request for  specific pokemon

--- api_pokemon_bmi/pokemon_api.py
+++ api_pokemon_bmi/pokemon_api.py
@@ -1,1 +1,16 @@
 import random
+import requests
+
+
+def get_pokemon_with_id(id):
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id}')
+    pokemon = response.json()
+    return {
+        'Name': pokemon['name'].capitalize(),
+        'Height': pokemon['height'],
+        'Weight': pokemon['weight'],
+    }
+
+
+if __name__ == '__main__':
+    print(get_pokemon_with_id(1))
